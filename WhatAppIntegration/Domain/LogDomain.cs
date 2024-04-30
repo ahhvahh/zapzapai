b10186322fb11d4ea3330623fdb7326ad9c77aa7
WildBillBiccup <84429613+ahhvahh@users.noreply.github.com>
2024-04-30 10:01:05 -0300

    Primeira versão

---
+++ WhatAppIntegration/Domain/LogDomain.cs
@@ -0,0 +1,15 @@
+﻿namespace WhatAppIntegration.Domain
+{
+    internal class LogDomain
+    {
+        private string LogFilePath => Path.Combine(AppContext.BaseDirectory, $"LogWhatsApp-{DateTime.Now:yyyy-MM-dd}.txt");
+
+        internal void LogMessage(string from, string body)
+        {
+            using (StreamWriter writer = new StreamWriter(LogFilePath, true))
+            {
+                writer.WriteLine($"Timestamp: {DateTime.Now} - From: {from} - Message: {body}");
+            }
+        }
+    }
+}
